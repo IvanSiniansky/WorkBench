61639c31357102fb6df3ce38b3ff8b2d5d1650af
IvanSiniansky <136857927+IvanSiniansky@users.noreply.github.com>
2023-06-18 23:52:06 +0300

    Delete Program1 directory

--- Program1/src/validations/CheckSymbolsValidation.java
+++
@@ -1,9 +0,0 @@
-package validations;
-
-public class CheckSymbolsValidation extends AbstractValidation {
-
-    @Override
-    boolean isValid() {
-        return this.validatedValue.matches("^[a-zA-Z0-9_]*+ [a-zA-Z0-9_]*$");
-    }
-}
